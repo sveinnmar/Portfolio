9c77c44c0b88a180f9dd8f4f78bb9794686ec743
Sveinn Már <sveinmar@gmail.com>
2021-08-13 01:01:09 +0000

    Making navbar cooler and changing color theme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-import CustomBtn from './CustomBtn';
 import "./NavBar.css";
 import { Button } from "./Btn"
 
@@ -29,35 +28,30 @@ function NavBar() {
         <>
             <nav className="navbar">
                 <div className="navbarContainer">
-                    <Link to="/" className="navbarLogo" onClick={closeMobileMenu}>
-                        Sveinn <i class="fas fa-dice-d20" />
-                    </Link>
                     <div className="menuIcon" onClick={handleClick}>
                         <i className={hamburgerClick ? "fas fa-times" : "fas fa-bars"} />
                     </div>
+                    <Link to="/" className="navbarLogo" onClick={closeMobileMenu}>
+                        Sveinn <i class="fas fa-dice-d20" />
+                    </Link>
                     <ul className={hamburgerClick ? "navMenu active" : "navMenu"}>
                         <li className="navItem">
-                            <Link to="/about" className="navLinks" onClick={closeMobileMenu}>
+                            <Link to="/about" className="navLinks about" onClick={closeMobileMenu}>
                                 About
                             </Link>
                         </li>
                         <li className="navItem">
-                            <Link to="/portfolio" className="navLinks" onClick={closeMobileMenu}>
+                            <Link to="/portfolio" className="navLinks portfolio" onClick={closeMobileMenu}>
                                 Portfolio
                             </Link>
                         </li>
                         <li className="navItem">
-                            <Link to="/contact" className="navLinks" onClick={closeMobileMenu}>
+                            <Link to="/contact" className="navLinks contact" onClick={closeMobileMenu}>
                                 Contact
                             </Link>
                         </li>
-                        <li className="navItem">
-                            <Link to="/" className="navLinks" onClick={closeMobileMenu}>
-                                <CustomBtn text="Download CV" />
-                            </Link>
-                        </li>
                     </ul>
-                    {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
+                    {button && <Button buttonStyle="btn--outline">CV</Button>}
                 </div>
             </nav>
         </>
